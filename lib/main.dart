213ba302f7e2c02e4fa9dd7581b1ad41118c351a
neelakandank <neelakandank@coresolutionsinc.com>
2025-06-24 14:57:05 -0400

    main is update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nutrio/src/provider/theme_provider.dart';
 import 'package:nutrio/src/theme/app_theme.dart';
 import 'package:nutrio/src/view/onboard/onboarding_screen.dart';
+
+import 'src/core/constants/app_constant.dart';
 
 void main() {
   runApp(
@@ -12,21 +15,17 @@ void main() {
   );
 }
 
-// Convert NutriOApp to a ConsumerWidget to access providers.
 class NutriOApp extends ConsumerWidget {
   const NutriOApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // Watch the themeProvider to get the current theme mode.
     final themeMode = ref.watch(themeProvider);
 
     return MaterialApp(
-      title: 'NutriO',
-      // Set the light and dark themes.
+      title: AppConstants.appName,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      // Use the themeMode from the provider to control which theme is active.
       themeMode: themeMode,
       home: const OnboardingScreen(),
       debugShowCheckedModeBanner: false,
