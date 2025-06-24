f984e3791585e7659ed0f82819141998ca3119fc
neelakandank <neelakandank@coresolutionsinc.com>
2025-06-24 14:56:04 -0400

    app _constant updated

---
+++ lib/src/core/constants/app_constant.dart
@@ -0,0 +1,39 @@
+// lib/src/util/app_constants.dart
+
+class AppConstants {
+  // App
+  static const String appName = 'NutriO';
+
+  // Themes
+  static const String toggleTheme = 'Toggle Theme';
+
+  // Onboarding
+  static const String onboardingComplete =
+      'Onboarding Complete. Navigating to HomeScreen.';
+  static const String finalData = 'Final Data: ';
+  static const String ageLabel = 'Age: ';
+  static const String genderLabel = 'Gender: ';
+  static const String heightLabel = 'Height: ';
+  static const String weightLabel = 'Weight: ';
+  static const String whatIsYourAge = 'What is your age?';
+  static const String years = 'years';
+  static const String tellUsAboutYou = 'Tell us about you';
+  static const String male = 'Male';
+  static const String female = 'Female';
+  static const String other = 'Other';
+  static const String whatIsYourHeight = 'What is your height?';
+  static const String whatIsYourWeight = 'What is your weight?';
+  static const String cm = 'cm';
+  static const String ft = 'ft';
+  static const String kg = 'kg';
+  static const String lb = 'lb';
+
+  // HomeScreen
+  static const String whatsOnYourPlate = "What's on your plate?";
+  static const String addAPhoto =
+      'Add a photo to get a nutritional breakdown of your meal.';
+  static const String takeAPicture = 'Take a picture';
+  static const String addFromGallery = 'Add from gallery';
+  static const String cameraButtonPressed = 'Camera button pressed';
+  static const String galleryButtonPressed = 'Gallery button pressed';
+}
